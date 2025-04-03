50e7a66181acb6e94344ffdbc2e5acfa859ad37f
Leboucq Thomas <thomas.leboucq@epitech.eu>
2025-04-03 12:53:43 +0200

    feat (parsing): add the proto of the parsing's fonctions

--- include/amazed.h
+++ include/amazed.h
@@ -7,7 +7,22 @@
 
 #ifndef AMAZED_H
     #define AMAZED_H
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include "vectors.h"
+
+typedef struct maze_s {
+    int num_robots;
+    vector_t *rooms;
+    vector_t *tunnels;
+    char *start_room;
+    char *end_room;
+} maze_t;
 
 int amazed(int const argc, char const *const argv[]);
 char *read_file(void);
+maze_t *parse_maze(void);
+char *my_strchr(const char *s, int c);
+
 #endif /* AMAZED_H*/
